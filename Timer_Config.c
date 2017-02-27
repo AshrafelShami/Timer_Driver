ef08171752fe706d90867493f19467ec588d372d
Ashraf Reda Mohamed <eng.ashrafreda@gmail.com>
2017-02-27 17:15:50 +0200

    Update Timer_Config.c

--- Timer_Config.c
+++ Timer_Config.c
@@ -7,26 +7,26 @@
   Purpose:  Initialize, Timer registers for ATmega32.
   Entry:    ( Struct_Timer Timer_Config ) a STRUCT that setup the following.
 							1) Adjust the Pre-Scaler.
-							6) Enable Timer overflow and Timer Compare match Interrupts. 
+							2) Enable Timer overflow and Timer Compare match Interrupts. 
   Exit:     no parameters
 ********************************************************************************************/
 void Timer_Init( Struct_Timer Timer_Config ){
 	switch(Timer_Config.Prescaler_Select){
-		case No_Clock_Source				: TCCRm &=~(1 << CSm2); TCCRm &=~(1 << CSm1); TCCRm &=~(1 << CSm0); break;
-		case Division_Factor_OFF			: TCCRm &=~(1 << CSm2); TCCRm &=~(1 << CSm1); TCCRm |= (1 << CSm0); break;
-		case Division_Factor_8				: TCCRm &=~(1 << CSm2); TCCRm |= (1 << CSm1); TCCRm &=~(1 << CSm0); break;
-		case Division_Factor_64				: TCCRm &=~(1 << CSm2); TCCRm |= (1 << CSm1); TCCRm |= (1 << CSm0); break;
-		case Division_Factor_256			: TCCRm |= (1 << CSm2); TCCRm &=~(1 << CSm1); TCCRm &=~(1 << CSm0); break;
-		case Division_Factor_1024			: TCCRm |= (1 << CSm2); TCCRm &=~(1 << CSm1); TCCRm |= (1 << CSm0); break;
+		case No_Clock_Source			: TCCRm &=~(1 << CSm2); TCCRm &=~(1 << CSm1); TCCRm &=~(1 << CSm0); break;
+		case Division_Factor_OFF		: TCCRm &=~(1 << CSm2); TCCRm &=~(1 << CSm1); TCCRm |= (1 << CSm0); break;
+		case Division_Factor_8			: TCCRm &=~(1 << CSm2); TCCRm |= (1 << CSm1); TCCRm &=~(1 << CSm0); break;
+		case Division_Factor_64			: TCCRm &=~(1 << CSm2); TCCRm |= (1 << CSm1); TCCRm |= (1 << CSm0); break;
+		case Division_Factor_256		: TCCRm |= (1 << CSm2); TCCRm &=~(1 << CSm1); TCCRm &=~(1 << CSm0); break;
+		case Division_Factor_1024		: TCCRm |= (1 << CSm2); TCCRm &=~(1 << CSm1); TCCRm |= (1 << CSm0); break;
 		case External_Clk_Src_Falling_egde	: TCCRm |= (1 << CSm2); TCCRm |= (1 << CSm1); TCCRm &=~(1 << CSm0); break;
 		case External_Clk_Src_Rising_egde	: TCCRm |= (1 << CSm2); TCCRm |= (1 << CSm1); TCCRm |= (1 << CSm0); break;
-		default								: TCCRm |= (1 << CSm2); TCCRm &=~(1 << CSm1); TCCRm |= (1 << CSm0); break;	
+		default					: TCCRm |= (1 << CSm2); TCCRm &=~(1 << CSm1); TCCRm |= (1 << CSm0); break;	
 	}
 	
 	switch(Timer_Config.Interrupt_Set){
 		case Timer_Overflow_Enable : TIMSK |= (1 << TOIEm); break;
 		case Timer_Compare_Enable  : TIMSK |= (1 << OCIEm); break;
-		default					   : TIMSK &=~(1 << TOIEm); TIMSK &=~(1 << OCIEm); break;
+		default			   : TIMSK &=~(1 << TOIEm); TIMSK &=~(1 << OCIEm); break;
 	}
 }
 
